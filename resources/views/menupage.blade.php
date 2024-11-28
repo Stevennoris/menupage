f6a84cfb389bf35bbc62e5318f9d9b50c063602a
Stevennoris <stevennoris23@gmail.com>
2024-11-28 15:34:16 +0700

    create card menu and cart list 30%

--- resources/views/menupage.blade.php
+++ resources/views/menupage.blade.php
@@ -3,9 +3,72 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>card page</title>
+    <link rel="stylesheet" href="style/style.css" >
 </head>
 <body>
-    <h1></h1>
+    <section class="menu">
+        
+        <div class="cardlist">
+            <div class="resto"></div>
+            <div class="card">test</div>
+            <div class="card">anj</div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+        </div>
+        <div class="cart" >
+            <div class="headercart" style="position: sticky;">
+                <div class="backtolist">X</div>
+                <div class="carttitle">
+                    <h1>CART</h1>
+                    <h2>McDonald's, Sentul City</h2>
+                </div>
+            </div>
+
+            <div class="cartmid" style="overflow-y: scroll; overflow-x: hidden; scrollbar-width: none; height: 60%;">
+    
+                <div class="quantity" style="width: 100%; height: 100px; background-color: navajowhite; ">
+                    p
+                </div>
+                
+                <div class="quantity" style="width: 100%; height: 100px; background-color: navajowhite; ">
+                    p
+                </div>
+                <div class="quantity" style="width: 100%; height: 100px; background-color: navajowhite; ">
+                    p
+                </div>
+                <div class="quantity" style="width: 100%; height: 100px; background-color: navajowhite; ">
+                    p
+                </div>
+                
+                <div class="quantity" style="width: 100%; height: 100px; background-color: navajowhite; ">
+                    p
+                </div>
+                <div class="quantity" style="width: 100%; height: 100px; background-color: navajowhite; ">
+                    p
+                </div>
+                <div class="quantity" style="width: 100%; height: 100px; background-color: navajowhite; ">
+                    p
+                </div>
+                <div class="quantity" style="width: 100%; height: 100px; background-color: navajowhite; ">
+                    p
+                </div>
+                <div class="quantity" style="width: 100%; height: 100px; background-color: navajowhite; ">
+                    p
+                </div>
+                <div class="picturemenu"></div>
+                <div class="descmenu"></div>
+            </div>
+        </div>
+        <div class="optionallist"></div>
+    </section>
 </body>
 </html>
